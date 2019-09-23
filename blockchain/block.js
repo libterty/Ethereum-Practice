3b369b002612a0327fa1635574fb110bee28471f
dreak603 <dreak603@gmail.com>
2019-09-23 22:13:10 +0800

    finish mineBlock fn

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,7 +1,9 @@
 const { GENSIS_DATA } = require('../config');
+const { keccakHash } = require('../util');
 
 const HASH_LENGTH = 64;
 const MAX_HASH_VALUE = parseInt('f'.repeat(HASH_LENGTH), 16);
+const MAX_NONCE_VALUE = 2 * 64;
 
 class Block {
   constructor({ blockHeaders }) {
@@ -23,7 +25,38 @@ class Block {
     return '0'.repeat(HASH_LENGTH - value.length) + value;
   }
 
-  static mineBlock({ lastBlock, beneficiary }) {}
+  static mineBlock({ lastBlock, beneficiary }) {
+    // temp header & nonce value will calculate actual hash that tries to meet the difficulty requirement.
+    // if the hash found by combining header and nonce val falls under target than the block is valid, then create base on truncatedBlockHeaders.
+    const target = Block.calculateBlockTargetHash({ lastBlock });
+    let timestamp, truncatedBlockHeaders, header, nonce, underTargetHash;
+
+    do {
+      timestamp = Date.now();
+      truncatedBlockHeaders = {
+        parentHash: keccakHash(lastBlock.blockHeaders),
+        beneficiary,
+        difficulty: lastBlock.blockHeaders.difficulty + 1,
+        number: lastBlock.blockHeaders.number + 1,
+        timestamp
+      };
+      header = keccakHash(truncatedBlockHeaders);
+      nonce = Math.floor(Math.random() * MAX_NONCE_VALUE);
+
+      underTargetHash = keccakHash(header + nonce);
+    } while (underTargetHash > target);
+
+    // console.log('underTargetHash', underTargetHash);
+    // console.log('target', target);
+
+    // this eq Block
+    return new this({
+      blockHeaders: {
+        ...truncatedBlockHeaders,
+        nonce
+      }
+    });
+  }
 
   static genesis() {
     return new this(GENSIS_DATA); // this eq Block
@@ -31,3 +64,9 @@ class Block {
 }
 
 module.exports = Block;
+
+// const block = Block.mineBlock({
+//   lastBlock: Block.genesis(),
+//   beneficiary: 'foo'
+// });
+// console.log('block', block);
--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,21 +1,47 @@
 const Block = require('./block');
+const { keccakHash } = require('../util')
 
 describe('Block', () => {
-  describe('calculateBlockTargetHash()', () => {
-    it('calculates the maximum hash when the last block difficult is 1', () => {
-      expect(
-        Block.calculateBlockTargetHash({
-          lastBlock: { blockHeaders: { difficulty: 1 } }
-        })
-      ).toEqual('f'.repeat(64));
+    describe('calculateBlockTargetHash()', () => {
+        it('calculates the maximum hash when the last block difficult is 1', () => {
+            expect(
+                Block.calculateBlockTargetHash({
+                    lastBlock: { blockHeaders: { difficulty: 1 } }
+                })
+            ).toEqual('f'.repeat(64));
+        });
+        // '000foo' > 'bason' ? 1 : 0
+        it('calculates a low hash value when the last block difficulty is high', () => {
+            expect(
+                Block.calculateBlockTargetHash({
+                    lastBlock: { blockHeaders: { difficulty: 500 } }
+                }) < '1'
+            ).toBe(true);
+        });
+
+        describe('mineBlock()', () => {
+            let lastBlock, mineBlock;
+
+            beforeEach(() => {
+                lastBlock = Block.genesis();
+                mineBlock = Block.mineBlock({ lastBlock, beneficiary: 'beneficiary' });
+            });
+
+            it('mines a block', () => {
+                expect(mineBlock).toBeInstanceOf(Block);
+            });
+
+            it('mines a block that meets the proof of work requirement', () => {
+                const target = Block.calculateBlockTargetHash({ lastBlock })
+                const { blockHeaders } = mineBlock
+                const { nonce } = blockHeaders
+                const truncatedBlockHeaders = {...blockHeaders }
+                delete truncatedBlockHeaders.nonce
+                const header = keccakHash(truncatedBlockHeaders)
+                const underTargetHash = keccakHash(header + nonce)
+
+                expect(underTargetHash < target).toBe(true)
+            })
+        });
     });
-    // '000foo' > 'bason' ? 1 : 0
-    it('calculates a low hash value when the last block difficulty is high', () => {
-      expect(
-        Block.calculateBlockTargetHash({
-          lastBlock: { blockHeaders: { difficulty: 500 } }
-        }) < '1'
-      ).toBe(true);
-    });
-  });
 });
