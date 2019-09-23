d73b7233fdb658a6e62181a9ba5ad22a1eb5daa1
dreak603 <dreak603@gmail.com>
2019-09-23 21:02:58 +0800

    init MineBlock fn

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,11 +1,29 @@
 const { GENSIS_DATA } = require('../config');
 
+const HASH_LENGTH = 64;
+const MAX_HASH_VALUE = parseInt('f'.repeat(HASH_LENGTH), 16);
+
 class Block {
   constructor({ blockHeaders }) {
     this.blockHeaders = blockHeaders;
   }
 
-  static mineBlock({ lastBlock }) {}
+  static calculateBlockTargetHash({ lastBlock }) {
+    // parseInt('f'.repeat(HASH_LENGTH), 16).toString(16).length // 65
+    const value = (MAX_HASH_VALUE / lastBlock.blockHeaders.difficulty).toString(
+      16
+    );
+
+    // Remain 64 length consistency, prevent large return
+    if (value.length > HASH_LENGTH) {
+      return 'f'.repeat(HASH_LENGTH);
+    }
+
+    // Remain 64 length consistency, prevent small return
+    return '0'.repeat(HASH_LENGTH - value.length) + value;
+  }
+
+  static mineBlock({ lastBlock, beneficiary }) {}
 
   static genesis() {
     return new this(GENSIS_DATA); // this eq Block
--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const GENSIS_DATA = {
   blockHeaders: {
     parentHash: '--genesis-parent-hash--',
     beneficiary: '--genesis-beneficiary--',
-    diffculty: 1,
+    difficulty: 1,
     number: 0,
     timestamp: '--genesis-timestamp--',
     nonce: 0
