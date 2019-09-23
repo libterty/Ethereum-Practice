f4583ff6d016b9da28c6c8c90601363fee784940
dreak603 <dreak603@gmail.com>
2019-09-24 01:03:12 +0800

    add Block validation

--- blockchain/block.js
+++ blockchain/block.js
@@ -3,78 +3,122 @@ const { keccakHash } = require('../util');
 
 const HASH_LENGTH = 64;
 const MAX_HASH_VALUE = parseInt('f'.repeat(HASH_LENGTH), 16);
-const MAX_NONCE_VALUE = 2 * 64;
+const MAX_NONCE_VALUE = 2 ** 64;
 
 class Block {
-    constructor({ blockHeaders }) {
-        this.blockHeaders = blockHeaders;
+  constructor({ blockHeaders }) {
+    this.blockHeaders = blockHeaders;
+  }
+
+  static calculateBlockTargetHash({ lastBlock }) {
+    // parseInt('f'.repeat(HASH_LENGTH), 16).toString(16).length // 65
+    const value = (MAX_HASH_VALUE / lastBlock.blockHeaders.difficulty).toString(
+      16
+    );
+
+    // Remain 64 length consistency, prevent large return
+    if (value.length > HASH_LENGTH) {
+      return 'f'.repeat(HASH_LENGTH);
     }
 
-    static calculateBlockTargetHash({ lastBlock }) {
-        // parseInt('f'.repeat(HASH_LENGTH), 16).toString(16).length // 65
-        const value = (MAX_HASH_VALUE / lastBlock.blockHeaders.difficulty).toString(
-            16
+    // Remain 64 length consistency, prevent small return
+    return '0'.repeat(HASH_LENGTH - value.length) + value;
+  }
+
+  static adjustDifficulty({ lastBlock, timestamp }) {
+    const { difficulty } = lastBlock.blockHeaders;
+
+    if (timestamp - lastBlock.blockHeaders.timestamp > MINE_RATE) {
+      return difficulty - 1;
+    }
+
+    if (difficulty < 1) {
+      return 1;
+    }
+
+    return difficulty + 1;
+  }
+
+  static mineBlock({ lastBlock, beneficiary }) {
+    // temp header & nonce value will calculate actual hash that tries to meet the difficulty requirement.
+    // if the hash found by combining header and nonce val falls under target than the block is valid, then create base on truncatedBlockHeaders.
+    const target = Block.calculateBlockTargetHash({ lastBlock });
+    let timestamp, truncatedBlockHeaders, header, nonce, underTargetHash;
+
+    do {
+      timestamp = Date.now();
+      truncatedBlockHeaders = {
+        parentHash: keccakHash(lastBlock.blockHeaders),
+        beneficiary,
+        difficulty: Block.adjustDifficulty({ lastBlock, timestamp }),
+        number: lastBlock.blockHeaders.number + 1,
+        timestamp
+      };
+      header = keccakHash(truncatedBlockHeaders);
+      nonce = Math.floor(Math.random() * MAX_NONCE_VALUE);
+
+      underTargetHash = keccakHash(header + nonce);
+    } while (underTargetHash > target);
+
+    // console.log('underTargetHash', underTargetHash);
+    // console.log('target', target);
+
+    // this eq Block
+    return new this({
+      blockHeaders: {
+        ...truncatedBlockHeaders,
+        nonce
+      }
+    });
+  }
+
+  static genesis() {
+    return new this(GENSIS_DATA); // this eq Block
+  }
+
+  static validateBlock({ lastBlock, block }) {
+    return new Promise((resolve, reject) => {
+      if (keccakHash(block) === keccakHash(Block.genesis())) {
+        return resolve();
+      }
+
+      if (
+        keccakHash(lastBlock.blockHeaders) !== block.blockHeaders.parentHash
+      ) {
+        return reject(
+          new Error("The parent hash must be a hash of the last block's header")
         );
+      }
 
-        // Remain 64 length consistency, prevent large return
-        if (value.length > HASH_LENGTH) {
-            return 'f'.repeat(HASH_LENGTH);
-        }
+      if (block.blockHeaders.number !== lastBlock.blockHeaders.number + 1) {
+        return reject(new Error('The block mush increment the number by 1'));
+      }
 
-        // Remain 64 length consistency, prevent small return
-        return '0'.repeat(HASH_LENGTH - value.length) + value;
-    }
+      if (
+        Math.abs(
+          lastBlock.blockHeaders.difficulty - block.blockHeaders.difficulty
+        ) > 1
+      ) {
+        return reject(new Error('The difficulty must only adjust by 1'));
+      }
 
-    static adjustDifficulty({ lastBlock, timestamp }) {
-        const { difficulty } = lastBlock.blockHeaders
+      const target = Block.calculateBlockTargetHash({ lastBlock });
+      const { blockHeaders } = block;
+      const { nonce } = blockHeaders;
+      const truncatedBlockHeaders = { ...blockHeaders };
+      delete truncatedBlockHeaders.nonce;
+      const header = keccakHash(truncatedBlockHeaders);
+      const underTargetHash = keccakHash(header + nonce);
 
-        if ((timestamp - lastBlock.blockHeaders.timestamp) > MINE_RATE) {
-            return difficulty - 1
-        }
+      if (underTargetHash > target) {
+        return reject(
+          new Error('The block does not meet the proof of work requirement')
+        );
+      }
 
-        if (difficulty < 1) {
-            return 1
-        }
-
-        return difficulty + 1
-    }
-
-    static mineBlock({ lastBlock, beneficiary }) {
-        // temp header & nonce value will calculate actual hash that tries to meet the difficulty requirement.
-        // if the hash found by combining header and nonce val falls under target than the block is valid, then create base on truncatedBlockHeaders.
-        const target = Block.calculateBlockTargetHash({ lastBlock });
-        let timestamp, truncatedBlockHeaders, header, nonce, underTargetHash;
-
-        do {
-            timestamp = Date.now();
-            truncatedBlockHeaders = {
-                parentHash: keccakHash(lastBlock.blockHeaders),
-                beneficiary,
-                difficulty: Block.adjustDifficulty({ lastBlock, timestamp }),
-                number: lastBlock.blockHeaders.number + 1,
-                timestamp
-            };
-            header = keccakHash(truncatedBlockHeaders);
-            nonce = Math.floor(Math.random() * MAX_NONCE_VALUE);
-
-            underTargetHash = keccakHash(header + nonce);
-        } while (underTargetHash > target);
-
-        // console.log('underTargetHash', underTargetHash);
-        // console.log('target', target);
-
-        // this eq Block
-        return new this({
-            blockHeaders: {
-                ...truncatedBlockHeaders,
-                nonce
-            }
-        });
-    }
-
-    static genesis() {
-        return new this(GENSIS_DATA); // this eq Block
-    }
+      return resolve();
+    });
+  }
 }
 
 module.exports = Block;
--- blockchain/index.js
+++ blockchain/index.js
@@ -1,28 +1,39 @@
 const Block = require('./block');
 
 class Blockchain {
-    constructor() {
-        this.chain = [Block.genesis()];
-    }
+  constructor() {
+    this.chain = [Block.genesis()];
+  }
 
-    addBlock({ block }) {
-        this.chain.push(block)
-    }
+  addBlock({ block }) {
+    return new Promise((resolve, reject) => {
+      Block.validateBlock({
+        lastBlock: this.chain[this.chain.length - 1],
+        block
+      })
+        .then(() => {
+          this.chain.push(block);
+
+          return resolve();
+        })
+        .catch(error => reject(error));
+    });
+  }
 }
 
 module.exports = Blockchain;
 
-const blockchain = new Blockchain()
+const blockchain = new Blockchain();
 
 for (let i = 0; i < 1000; i++) {
-    const lastBlock = blockchain.chain[blockchain.chain.length - 1]
-    const block = Block.mineBlock({
-        lastBlock,
-        beneficiary: 'beneficiary'
-    })
-    blockchain.addBlock({ block })
+  const lastBlock = blockchain.chain[blockchain.chain.length - 1];
+  const block = Block.mineBlock({
+    lastBlock,
+    beneficiary: 'beneficiary'
+  });
+  blockchain.addBlock({ block });
 
-    console.log('block', block)
+  console.log('block', block);
 }
 
 // const blockchain = new Blockchain();
